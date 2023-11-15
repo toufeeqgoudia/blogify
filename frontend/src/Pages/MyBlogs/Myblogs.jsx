31a23bf837292b13bee84165921a2a463d59a792
toufeeqgoudia <toufeeqgoudia@gmail.com>
2023-11-15 17:10:45 +0200

    populated myblogs with blogs posted by user

--- frontend/src/Pages/MyBlogs/Myblogs.jsx
+++ frontend/src/Pages/MyBlogs/Myblogs.jsx
@@ -1,70 +1,61 @@
-import {FaEdit, FaTrash} from 'react-icons/fa'
+import { useState, useEffect } from "react";
+import { instance } from "../../utils/apiService";
+import { useAuth } from "../../Hooks/useAuth";
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { IoAddCircleOutline } from "react-icons/io5";
 
 const Myblogs = () => {
+  const [posts, setPosts] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    const getPostData = async () => {
+      const response = await instance.get(`/api/posts/`);
+      setPosts(response.data);
+    };
+
+    getPostData();
+  }, []);
+
+  const userPosts = posts.filter((post) => post.author.id === user.id);
+
   return (
     <div className="w-3/4 h-full p-5 flex flex-col ml-25">
       <h2 className="text-lg font-semibold self-center">my blogs.</h2>
       <hr />
+
       <div>
-
-        <div className='w-full flex'>
-          <div className="w-11/12 h-40 p-3 my-5 overflow-hidden border-2 border-slate-200 rounded-s-md">
-            <h3>Title</h3>
-            <h4>Author<span> - date posted</span></h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempore repellendus officia atque tenetur illum laborum iure sequi voluptate provident, distinctio consequuntur ut nemo. Ullam quisquam ut neque exercitationem maiores.
-            Natus, voluptatum. Quas, modi fugiat consectetur earum impedit odio magnam suscipit at laudantium vel deserunt perspiciatis odit. Amet quo laudantium dicta mollitia consequatur. Facilis, provident! Vel quasi voluptatum nostrum voluptatibus.
-            Provident hic sed aliquam eius eaque id architecto doloribus quo harum quam similique officiis voluptates quia, enim dolor, voluptatem corrupti magni eos officia magnam qui explicabo obcaecati. Dignissimos, asperiores excepturi.
-            Cumque, sit. Eum expedita quisquam provident aliquam! Possimus veritatis nemo alias error enim saepe aut odit maiores. Sequi facere voluptates id totam ipsum impedit dolore, omnis ratione tempore aperiam maxime.
-            Aperiam expedita ipsum omnis neque obcaecati ullam placeat perspiciatis, cumque autem corrupti non delectus unde voluptatum corporis animi accusantium magnam dicta aut recusandae consequuntur debitis illum officiis similique molestiae? Dolore.</p>
+        {userPosts.map((post) => (
+          <div key={post.id} className="w-full flex">
+            <div className="w-full h-40 p-3 my-5 overflow-hidden border-2 border-slate-200 rounded-s-md cursor-pointer">
+              <div className="flex items-center my-1">
+                <img
+                  src={post.author.profile_img}
+                  className="w-7 h-7 rounded-full border-2 border-slate-200"
+                />
+                <h3 className="text-lg font-bold ml-4">
+                  {post.author.username}
+                </h3>
+              </div>
+              <h4 className="text-lg font-medium">
+                {post.title}
+                <span className="text-xs"> - {post.date_posted}</span>
+              </h4>
+              <p className="text-sm">{post.content}</p>
+            </div>
+            <div className="w-1/12 h-40 my-5 flex flex-col justify-evenly items-center bg-slate-200 rounded-e-md">
+              <FaEdit className="cursor-pointer" />
+              <FaTrash className="text-red-600 cursor-pointer" />
+            </div>
           </div>
-          <div className='w-1/12 h-40 my-5 flex flex-col justify-evenly items-center bg-slate-200 rounded-e-md'>
-            <FaEdit className='cursor-pointer' />
-            <FaTrash className='text-red-600 cursor-pointer' />
-          </div>
-        </div>
-
-        <div className='w-full flex'>
-          <div className="w-11/12 h-40 p-3 my-5 overflow-hidden border-2 border-slate-200 rounded-s-md">
-            <h3>Title</h3>
-            <h4>Author<span> - date posted</span></h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempore repellendus officia atque tenetur illum laborum iure sequi voluptate provident, distinctio consequuntur ut nemo. Ullam quisquam ut neque exercitationem maiores.
-            Natus, voluptatum. Quas, modi fugiat consectetur earum impedit odio magnam suscipit at laudantium vel deserunt perspiciatis odit. Amet quo laudantium dicta mollitia consequatur. Facilis, provident! Vel quasi voluptatum nostrum voluptatibus.
-            Provident hic sed aliquam eius eaque id architecto doloribus quo harum quam similique officiis voluptates quia, enim dolor, voluptatem corrupti magni eos officia magnam qui explicabo obcaecati. Dignissimos, asperiores excepturi.
-            Cumque, sit. Eum expedita quisquam provident aliquam! Possimus veritatis nemo alias error enim saepe aut odit maiores. Sequi facere voluptates id totam ipsum impedit dolore, omnis ratione tempore aperiam maxime.
-            Aperiam expedita ipsum omnis neque obcaecati ullam placeat perspiciatis, cumque autem corrupti non delectus unde voluptatum corporis animi accusantium magnam dicta aut recusandae consequuntur debitis illum officiis similique molestiae? Dolore.</p>
-          </div>
-          <div className='w-1/12 h-40 my-5 flex flex-col justify-evenly items-center bg-slate-200 rounded-e-md'>
-            <FaEdit className='cursor-pointer' />
-            <FaTrash className='text-red-600 cursor-pointer' />
-          </div>
-        </div>
-
-        <div className='w-full flex'>
-          <div className="w-11/12 h-40 p-3 my-5 overflow-hidden border-2 border-slate-200 rounded-s-md">
-            <h3>Title</h3>
-            <h4>Author<span> - date posted</span></h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tempore repellendus officia atque tenetur illum laborum iure sequi voluptate provident, distinctio consequuntur ut nemo. Ullam quisquam ut neque exercitationem maiores.
-            Natus, voluptatum. Quas, modi fugiat consectetur earum impedit odio magnam suscipit at laudantium vel deserunt perspiciatis odit. Amet quo laudantium dicta mollitia consequatur. Facilis, provident! Vel quasi voluptatum nostrum voluptatibus.
-            Provident hic sed aliquam eius eaque id architecto doloribus quo harum quam similique officiis voluptates quia, enim dolor, voluptatem corrupti magni eos officia magnam qui explicabo obcaecati. Dignissimos, asperiores excepturi.
-            Cumque, sit. Eum expedita quisquam provident aliquam! Possimus veritatis nemo alias error enim saepe aut odit maiores. Sequi facere voluptates id totam ipsum impedit dolore, omnis ratione tempore aperiam maxime.
-            Aperiam expedita ipsum omnis neque obcaecati ullam placeat perspiciatis, cumque autem corrupti non delectus unde voluptatum corporis animi accusantium magnam dicta aut recusandae consequuntur debitis illum officiis similique molestiae? Dolore.</p>
-          </div>
-          <div className='w-1/12 h-40 my-5 flex flex-col justify-evenly items-center bg-slate-200 rounded-e-md'>
-            <FaEdit className='cursor-pointer' />
-            <FaTrash className='text-red-600 cursor-pointer' />
-          </div>
-        </div>
-
+        ))}
       </div>
 
-
-
-      <button className='p-3 w-32 flex self-end items-center justify-evenly text-white bg-emerald-500 fixed rounded-full bottom-10 right-10 z-10'>
-        <IoAddCircleOutline className='text-2xl font-bold text-white' /> create.
+      <button className="p-3 w-32 flex self-end items-center justify-evenly text-white bg-emerald-500 fixed rounded-full bottom-10 right-10 z-10">
+        <IoAddCircleOutline className="text-2xl font-bold text-white" /> create.
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Myblogs
+export default Myblogs;
